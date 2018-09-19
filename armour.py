42e55b75097193d656cbaf69d510c2e61c89dfc1
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-20 09:23:20 +1000

    Add function: acceleration

--- armour.py
+++ armour.py
@@ -9,3 +9,12 @@ grade_types = [{'Type I': '', 'Mass': 4.4, 'Velocity': 325.5, 'Acceleration': 13
                {'Type II': '', 'Mass': 9.1, 'Velocity': 393.5, 'Acceleration': 193553, 'Force': 1761},
                {'Type III': '', 'Mass': 9.6, 'Velocity': 847, 'Acceleration': 896761, 'Force': 8609},
                {'Type IV': '', 'Mass': 10.8, 'Velocity': 878, 'Acceleration': 963605, 'Force': 10407},]
+
+# Calculate acceleration
+def acceleration(velocity):
+    velocity = int(velocity)
+    distance = float(0.4)
+    acceleration = (0.5 * velocity**2) / (distance)
+    return acceleration
+
+
