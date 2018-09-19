3f60cba963f781de369234f5be748730504825bc
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-20 09:03:45 +1000

    Add list of dictionaries for body armour grade types

--- armour.py
+++ armour.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
+Body Armour Certification
 """
 
+# list of dictionaries for body armour grade types
+grade_types = [{'Type I': '', 'Mass': 4.4, 'Velocity': 325.5, 'Acceleration': 132438, 'Force': 583},
+               {'Type II': '', 'Mass': 9.1, 'Velocity': 393.5, 'Acceleration': 193553, 'Force': 1761},
+               {'Type III': '', 'Mass': 9.6, 'Velocity': 847, 'Acceleration': 896761, 'Force': 8609},
+               {'Type IV': '', 'Mass': 10.8, 'Velocity': 878, 'Acceleration': 963605, 'Force': 10407},]
