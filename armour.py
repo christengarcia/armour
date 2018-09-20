e12e449235f0ef6d2fcb6121f352a7d006146956
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-20 10:17:08 +1000

    Add function: force_newtons

--- armour.py
+++ armour.py
@@ -3,7 +3,7 @@
 """
 Body Armour Certification
 """
-
+import math
 # list of dictionaries for body armour grade types
 grade_types = [{'Type I': '', 'Mass': 4.4, 'Velocity': 325.5, 'Acceleration': 132438, 'Force': 583},
                {'Type II': '', 'Mass': 9.1, 'Velocity': 393.5, 'Acceleration': 193553, 'Force': 1761},
@@ -23,4 +23,13 @@ def force(mass, acceleration):
     mass = float(mass)
     force = mass * acceleration
     return force
+
+
+# Calculate force in newtons
+def force_newtons():
+    new_acceleration = acceleration(velocity)
+    new_force = force(mass, new_acceleration)
+    print(math.ceil(new_force/1000))
+    return (math.ceil(new_force/1000))
+
 
